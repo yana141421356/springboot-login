8b98ead7a597c8dba1b7158d2b69b76e94104853
yana141421356 <atsushi.yanai1977@gmail.com>
2024-11-17 10:39:32 +0900

    修正

--- src/main/java/com/example/springboot_login/config/SecurityConfig.java
+++ src/main/java/com/example/springboot_login/config/SecurityConfig.java
@@ -21,7 +21,6 @@ public class SecurityConfig {
         this.userDetailsService = userDetailsService;
     }
 
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
--- src/main/java/com/example/springboot_login/service/CustomUserDetailsService.java
+++ src/main/java/com/example/springboot_login/service/CustomUserDetailsService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 import com.example.springboot_login.entity.User;
 import com.example.springboot_login.repository.UserRepository;
 
+/**
+ * カスタムUserDetailsServiceの実装クラス。 ユーザー名に基づいてユーザーの詳細情報をロードします。
+ */
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
@@ -16,12 +19,22 @@ public class CustomUserDetailsService implements UserDetailsService {
         this.userRepository = userRepository;
     }
 
+    /**
+     * 指定されたユーザー名に基づいてユーザーの詳細情報をロードします。
+     * 
+     * @param username ユーザー名
+     * @return UserDetailsインスタンス
+     * @throws UsernameNotFoundException ユーザーが見つからない場合にスローされます
+     */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // ユーザー名に基づいてユーザーを検索
         User user = userRepository.findByUsername(username);
         if (user == null) {
+            // ユーザーが見つからない場合は例外をスロー
             throw new UsernameNotFoundException("User not found");
         }
+        // ユーザーの詳細情報を返す
         return org.springframework.security.core.userdetails.User.withUsername(user.getUsername())
                 .password(user.getPassword()).roles(user.getRole()).build();
     }
